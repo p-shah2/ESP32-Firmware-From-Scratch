25479224323652567e54ff77533557fb1e233bac
Priyanka Shah <priyankabirju.shah@sjsu.edu>
2025-04-18 13:55:39 -0700

    updating flash script

--- flash.sh
+++ flash.sh
@@ -4,3 +4,8 @@
 
 # simple flash script
 
+# Erase previous img since we're not using a bootloader/partitions etc 
+esptool.py --chip esp32 -p /dev/tty.usbserial-0001 erase_flash
+
+# Flash new image 
+esptool.py --chip esp32 -b 115200 -p /dev/tty.usbserial-0001 write_flash 0x10000 build/main.bin
